21730372df78fb74e130213ec8fa798b7ebd57c2
Abdulaz1zov1 <abdulazizovoyatillo94@gmail.com>
2022-12-02 09:12:56 +0500

    AS

---
+++ keys/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    BASE_URL: 'localhost',
+    MONGODB_URI: 'mongodb://localhost:27017/abcosystem',
+    SESSION_SECRET: 'asdasdasdasdasd',
+    SYSTEM_EMAIL: 'emailer5fgroup@gmail.com',
+    PASSWORD_EMAIL: 'r00t5fgroup'
+}
